17077305ef8dd1825f72f6f7eced4c25c85463e1
HenriqueMitsuo <henrique.informatica123@gmail.com>
2019-02-17 14:17:00 -0300

    Adionado estrutura para as Views

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -7,14 +7,22 @@ use Illuminate\Http\Request;
 class PagesController extends Controller
 {
     public function index(){
-        return view('pages.index');
+        // PASSANDO PARAMENTROS PARA VIEW
+        $title = 'Welcome to Laravel';
+        // return view('pages.index', compact('title'));
+        return view('pages.index')->with('title', $title);
     }
 
     public function about(){
-        return view('pages.about');
+        $title = 'About us';
+        return view('pages.about')->with('title', $title);
     }
 
     public function services(){
-        return view('pages.services');
+        $data = array(
+            'title' => 'Services',
+            'services' => ['Web desing', 'Programing', 'SEO']
+        );
+        return view('pages.services')->with($data);
     }
 }
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,12 +1,7 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>{{config('app.name', 'LaravelReference')}}</title>
-    </head>
-    <body>
-        <h1>Laravel App!</h1>
+@extends('layouts.app')
+    @section('content')
+        <h1>{{$title}}</h1>
         <p>Laravel Reference App</p>
-    </body>
-</html>
+    @endsection
+
+
