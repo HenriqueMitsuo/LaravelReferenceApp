1250d7aa1911f4d53d75548764e1d7ddcae85fa7
HenriqueMitsuo <henrique.informatica123@gmail.com>
2019-02-17 13:52:49 -0300

    Adicionado Controle de Rotas

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>{{config('app.name', 'LaravelReference')}}</title>
+    </head>
+    <body>
+        <h1>Laravel App!</h1>
+        <p>Laravel Reference App</p>
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,22 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/hello', function () {
+//     return '<h1>Hello World</h1>';
+// });
+
+// Route::get('/users/{id}/{name}', function ($id, $name) {
+//     return 'This is user: '.$id.' Name: '.$name;
+// });
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', 'PagesController@index');
+
+Route::get('/about', 'PagesController@about');
+
+Route::get('/services', 'PagesController@services');
+
+
