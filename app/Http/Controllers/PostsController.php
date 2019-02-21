115b850db236f84d93261b0aefedf1d1c1a874e1
HenriqueMitsuo <henrique.informatica123@gmail.com>
2019-02-21 15:37:49 -0300

    Adicionado Upload de arquivos

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 use App\Post;
 use BD; // PERMITE A UTILIZAÇÂO DE QUERYS SQL COMUNS
 
@@ -57,14 +58,35 @@ class PostsController extends Controller
         //CRIA REGRAS PARA A VALIDAÇÂO
         $this->validate($request, [
             'title' => 'required',
-            'body' => 'required'
+            'body' => 'required',
+            'cover_image' => 'image|nullable|max:1999'
         ]);
+
+        // UPLOAD DE IMAGEM
+        if($request->hasFile('cover_image'))
+        {
+            // NOME E EXTENÇÃO DA IMAGEM
+            $fileNameWithExt = $request->file('cover_image')->getClientOriginalName();
+            // SOMENTE NOME
+            $fileName = pathinfo($fileNameWithExt, PATHINFO_FILENAME);
+            // SOMENTE EXTENÇÂO
+            $extension = $request->file('cover_image')->getClientOriginalExtension();
+            // NOME QUE IRÀ PARA O BANCO
+            $fileNameToStore = $fileName.'_'.time().'.'.$extension;
+            // UPLOAD
+            $path = $request->file('cover_image')->storeAs('public/cover_images', $fileNameToStore);
+        }
+        else
+        {
+            $fileNameToStore = 'noimage.png';
+        }
 
         $post = new Post;
         // INSERE NO BANCO USANDO O TINKER
         $post->posts_title = $request->input('title');
         $post->posts_body = $request->input('body');
         $post->user_id = auth()->user()->id;
+        $post->cover_image = $fileNameToStore;
         $post->save();
 
         return redirect('/posts')->with('success', 'Post created!');
@@ -111,13 +133,33 @@ class PostsController extends Controller
         //CRIA REGRAS PARA A VALIDAÇÂO
         $this->validate($request, [
             'title' => 'required',
-            'body' => 'required'
+            'body' => 'required',
+            'cover_image' => 'image|nullable|max:1999'
         ]);
+
+        // UPLOAD DE IMAGEM
+        if($request->hasFile('cover_image'))
+        {
+            // NOME E EXTENÇÃO DA IMAGEM
+            $fileNameWithExt = $request->file('cover_image')->getClientOriginalName();
+            // SOMENTE NOME
+            $fileName = pathinfo($fileNameWithExt, PATHINFO_FILENAME);
+            // SOMENTE EXTENÇÂO
+            $extension = $request->file('cover_image')->getClientOriginalExtension();
+            // NOME QUE IRÀ PARA O BANCO
+            $fileNameToStore = $fileName.'_'.time().'.'.$extension;
+            // UPLOAD
+            $path = $request->file('cover_image')->storeAs('public/cover_images', $fileNameToStore);
+        }
 
         $post = Post::find($id);
         // INSERE NO BANCO USANDO O TINKER
         $post->posts_title = $request->input('title');
         $post->posts_body = $request->input('body');
+        if ($request->hasFile('cover_image'))
+        {
+            $post->cover_image = $fileNameToStore;
+        }
         $post->save();
 
         return redirect('/posts')->with('success', 'Post updated!');
@@ -136,6 +178,12 @@ class PostsController extends Controller
         {
             return redirect('/posts')->with('error', 'Unauthorized Page');
         }
+
+        if($post->cover_image != 'noimage.png')
+        {
+            Storage::delete('public/cover_images/'.$post->cover_image);
+        }
+
         $post->delete();
         return redirect('/posts')->with('success', 'Post removed!');
     }
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,22 +1,25 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="card container mt-3">
+    <div class="card mt-3">
+        <img src="/storage/cover_images/{{$post->cover_image}}" class="card-img" alt="...">
+
         <div class="card-body">
             <h1 class="card-title">{{$post->posts_title}}</h1>
-        <h6 class="card-subtitle mb-2 text-muted">Written on {{$post->created_at}} by {{$post->user->name}}</h6>
-        <div class="card-text">{!!$post->posts_body!!}</div>
-        <hr>
-        <a href="/posts" class="card-link btn btn-secondary">Go back</a>
-        @if (!Auth::guest())
-            @if (Auth::user()->id == $post->user->id)
-                <a href="/posts/{{$post->posts_id}}/edit" class="card-link btn btn-dark">Edit</a>
-                {!!Form::open(['action' => ['PostsController@destroy', $post->posts_id], 'method' => 'POST', 'class' => 'd-inline card-link'])!!}
-                    {{Form::hidden('_method', 'DELETE')}}
-                    {{Form::submit('Delete', ['class' => 'btn btn-danger text-right'])}}
-                {!!Form::close() !!}
+
+            <h6 class="card-subtitle mb-2 text-muted">Written on {{$post->created_at}} by {{$post->user->name}}</h6>
+            <div class="card-text">{!!$post->posts_body!!}</div>
+            <hr>
+            <a href="/posts" class="card-link btn btn-secondary">Go back</a>
+            @if (!Auth::guest())
+                @if (Auth::user()->id == $post->user->id)
+                    <a href="/posts/{{$post->posts_id}}/edit" class="card-link btn btn-dark">Edit</a>
+                    {!!Form::open(['action' => ['PostsController@destroy', $post->posts_id], 'method' => 'POST', 'class' => 'd-inline card-link'])!!}
+                        {{Form::hidden('_method', 'DELETE')}}
+                        {{Form::submit('Delete', ['class' => 'btn btn-danger text-right'])}}
+                    {!!Form::close() !!}
+                @endif
             @endif
-        @endif
         </div>
     </div>
 @endsection
